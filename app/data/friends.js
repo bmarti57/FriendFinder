168e724dbf5a5a1d2eaf4f8876f1204664105b59
Brian Martin <Brian Martin>
2017-08-30 19:44:28 -0400

    js and html updated

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+	{
+		name: 'Doug',
+		photo: 'https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr05/2013/7/12/13/enhanced-buzz-orig-30348-1373648544-22.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+		scores: [
+			'2',
+			'5',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'3',
+			'4',
+			'5'
+		]
+    },
+    {
+		name: 'Alf',
+		photo: 'https://vtbarcounsel.files.wordpress.com/2016/12/alf.jpg',
+		scores: [
+			'4',
+			'1',
+			'1',
+			'5',
+			'3',
+			'4',
+			'1',
+			'3',
+			'2',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = 8081;
+
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: true
+}));
+app.use(bodyParser.text());
+
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+
+
+app.listen(PORT, function() {
+    console.log("Listening on: " + PORT)
+});
+
+
