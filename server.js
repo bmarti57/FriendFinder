96cfce6cb310341d414a8e174514ebe2c009806c
Brian Martin <Brian Martin>
2017-08-31 19:33:43 -0400

    apiRoutes and links working

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var PORT = 8081;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
+
 
 app.listen(PORT);
 console.log("Listening on: " + PORT);
