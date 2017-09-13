a1c9ed8d9379f62eebf236058c7a518d0ee4237b
Brian Martin <Brian Martin>
2017-09-12 20:59:22 -0400

    dynamic port added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 8081;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
